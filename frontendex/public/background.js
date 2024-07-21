7a4b670aad2f2ccb8d4094a78762418dd0ff7fd9
Rana Dolui <ranadolui718@gmail.com>
2024-07-22 00:31:59 +0530

    Delete frontendex directory

--- frontendex/public/background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-    if (tab.url.includes("amazon.in")) {
-        chrome.storage.local.set({ amazonURL: tab.url });
-    } else {
-        chrome.storage.local.set({ amazonURL: "Not an Amazon URL." });
-    }
-  });
-  
